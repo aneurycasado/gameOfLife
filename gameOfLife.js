48e9eed6ad8702823b678aed3ebf25a41f440c8b
Aneury Casado <aneury.casado@gmail.com>
2015-08-02 23:15:48 -0400

    Finished Kata

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,23 +1,38 @@
-function nextGen(cells){
-  for(var i = 0; i < cells.length; i++){
-    for(var j = 0; j < cells.length; j++){
+
+function deepCopy(cells){
+  var newArray = [];
+  for(var i = 0; i < cells.length;i++){
+    var row = [];
+    for(var j = 0; j < cells[0].length;j++){
       var cell = cells[i][j];
-      var cell1 = cells[i-1][j-1];
-      var cell2 = cells[i-1][j];
-      var cell3 = cells[i-1][j+1];
-      var cell4 = cells[i][j-1];
-      var cell5 = cells[i][j+1];
-      var cell6 = cells[i+1][j-1];
-      var cell7 = cells[i+1][j];
-      var cell8 = cells[i+1][j+1];
-      if(cell1 === undefined) cell1 = 0;
-      if(cell2 === undefined) cell2 = 0;
-      if(cell3 === undefined) cell3 = 0;
-      if(cell4 === undefined) cell4 = 0;
-      if(cell5 === undefined) cell5 = 0;
-      if(cell6 === undefined) cell6 = 0;
-      if(cell7 === undefined) cell7 = 0;
-      if(cell8 === undefined) cell8 = 0;
+      row.push(cell);
+    }
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+function nextGen(cells){
+  var oldBoard = deepCopy(cells);
+  for(var i = 0; i < cells.length; i++){
+    for(var j = 0; j < cells[0].length; j++){
+      var cell = oldBoard[i][j];
+      if(i - 1 < 0 || j - 1 < 0) var cell1 = 0;
+      else var cell1 = oldBoard[i-1][j-1];
+      if(i - 1 < 0) var cell2 = 0;
+      else var cell2 = oldBoard[i-1][j];
+      if(i - 1 < 0 || j + 1 >= cells[0].length) var cell3 = 0;
+      else var cell3 = oldBoard[i-1][j+1];
+      if(j -1 < 0) var cell4 = 0;
+      else var cell4 = oldBoard[i][j-1];
+      if(j+1 >= cells[0].length) var cell5 = 0;
+      else var cell5 = oldBoard[i][j+1];
+      if(i+1 >=cells.length || j -1 < 0) var cell6 = 0;
+      else var cell6 = oldBoard[i+1][j-1];
+      if(i+1 >= cells.length) var cell7 = 0;
+      else var cell7 = oldBoard[i+1][j];
+      if(i+1 >= cells.length || j+1 >= cells[0].length) var cell8 = 0;
+      else var cell8 = oldBoard[i+1][j+1];
       var count = cell1 + cell2 + cell3 + cell4 + cell5 + cell6 + cell7 + cell8;
       if(cell === 1){
         if(count < 2) cells[i][j] = 0;
@@ -30,3 +45,5 @@ function nextGen(cells){
   }
   return cells;
 }
+
+console.log(nextGen([ [ 0, 1, 0 ], [ 0, 1, 0 ], [ 0, 1, 0 ] ]));
